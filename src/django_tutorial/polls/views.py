2906ebd530b7c08eda0412a1423098da3d955637
blackjack <kurawata414@gmail.com>
2025-01-24 19:29:54 +0900

    ✨ チュートリアル3 まで終わり

--- src/django_tutorial/polls/views.py
+++ src/django_tutorial/polls/views.py
@@ -1,6 +1,29 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+
+from .models import Question
 
 
-# ruff:noqa: ARG001, 必須のパラメータなので警告を無視
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # ハイフンありだと降順で並ぶ。ハイフンがないと昇順。
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse(f"You're voting on question {question_id}")
