81fb486ad17e2386cc9a529583929e243b75085c
blackjack <kurawata414@gmail.com>
2025-01-25 18:38:55 +0900

    ✨ チュートリアル8まで完了

--- src/django_tutorial/polls/admin.py
+++ src/django_tutorial/polls/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # 管理者画面でのフィールドの並び順を指定
+    # fields = ["pub_date", "question_text"]
+
+    # より構造化して admin の表示を制御
+    fieldsets = [
+        ("質問のタイトル", {"fields": ["question_text"]}),
+        ("質問の公開日", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    search_fields = ["question_text"]
+
+
+admin.site.register(Question, QuestionAdmin)
--- src/django_tutorial/polls/models.py
+++ src/django_tutorial/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -11,6 +12,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(boolean=True, ordering="pub_date", description="最近の質問かどうか")
     def was_published_recently(self):
         now = timezone.now()
         twenty_four_hours_ago = now - datetime.timedelta(days=1)
